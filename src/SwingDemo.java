c1fc28f90830c3640f1ceaa496c13b069a59713f
Mark Reardon <mcreardon@gmail.com>
2012-01-21 00:26:55 +0000

    added some buttons for testing :-)

--- src/SwingDemo.java
+++ src/SwingDemo.java
@@ -7,12 +7,21 @@
  */
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SwingDemo {
-    
+
+public class SwingDemo implements ActionListener{
+
+    JLabel jlab;
+
     SwingDemo() { 
     // 1st Create the JFrame container.
     JFrame jfrm = new JFrame("A very Simple Swing Program.");
+        
+    // Add flowlayout
+    jfrm.getContentPane().setLayout(new FlowLayout());
         
     // Initial size 
     jfrm.setSize(274, 100);
@@ -30,17 +39,27 @@ public class SwingDemo {
         
       
     // create a text based label
-    JLabel jlab = new JLabel(" Swing is the new GUI");
+    //JLabel jlab = new JLabel(" Swing is the new GUI");
 
     // add the label to the content pane
     
     jfrm.getContentPane().add(jbtnFirst);
     jfrm.getContentPane().add(jbtnSecond);
+
+    // create a text based label
+    jlab = new JLabel("Press a button");
     jfrm.getContentPane().add(jlab);
         
     // Add the label to the content pane
     jfrm.setVisible(true);
+    }
     
+    // We need to handle button action events
+    public void actionPerformed(ActionEvent ae) {
+    if ( ae.getActionCommand().equals("First"))
+        jlab.setText("first button was pressed");
+    else
+        jlab.setText("second button was pressed");
     }
     
     public static void main(String args[]) {
